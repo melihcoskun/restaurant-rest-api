8d16ec6488e72688ebe1c783857dc5f928d6a240
coskun <melihcoskunk@gmail.com>
2024-04-16 19:50:44 +0300

    Initial commit - created project folder structure and connected to database.

---
+++ src/main/java/com/melihcoskun/restaurant/SpringbootRestaurantRestApiApplication.java
@@ -0,0 +1,13 @@
+package com.melihcoskun.restaurant;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootRestaurantRestApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootRestaurantRestApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/melihcoskun/restaurant/SpringbootRestaurantRestApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.melihcoskun.restaurant;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootRestaurantRestApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
